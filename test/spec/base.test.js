8f93d693e76403bbe137144a6b63c8d753ee66eb
Thanasis Polychronakis <thanpolas@gmail.com>
2014-10-19 18:45:03 +0300

    expand base tests with error conditions

--- test/spec/base.test.js
+++ test/spec/base.test.js
@@ -4,22 +4,36 @@
 var Promise = require('bluebird');
 var chai = require('chai');
 var expect = chai.expect;
+var kansas = require('kansas');
 
 var tester = require('../lib/tester.lib');
 
 var kansasMetrics = require('../..');
 
 
-describe('Base API Surface', function() {
+describe.only('Base API Surface', function() {
   tester.init();
+
   it('should have expected exposed methods', function() {
     expect(kansasMetrics).to.be.a('function');
     expect(kansasMetrics.setup).to.be.a('function');
   });
+  it('should throw an error if no Kansas is defined', function() {
+    expect(kansasMetrics).to.throw(Error);
+    expect(kansasMetrics).to.throw(/No Kansas instance was defined/);
+  });
+  it('should throw an error if Kansas is not connected', function() {
+    kansasMetrics.setup(kansas);
+    expect(kansasMetrics).to.throw(Error);
+    expect(kansasMetrics).to.throw(/Kansas is not connected to Redis/);
+  });
+
   it('should return a Bluebird Promise when invoked', function() {
+    kansasMetrics.setup(this.kansas);
     expect(kansasMetrics()).to.be.instanceof(Promise);
   });
   it('should have expected methods when invoked', function() {
+    kansasMetrics.setup(this.kansas);
     var queryObj = kansasMetrics();
     expect(queryObj).to.be.an('object');
     expect(queryObj.user).to.be.a('function');
