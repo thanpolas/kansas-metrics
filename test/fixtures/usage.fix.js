8327dd1a46130758217be22ac69d16d492edc3f2
Thanasis Polychronakis <thanpolas@gmail.com>
2014-10-19 11:13:54 +0300

    Create usage fixtures

---
+++ test/fixtures/usage.fix.js
@@ -0,0 +1,83 @@
+/**
+ * @fileOverview Kansas Usage Fixtures.
+ */
+
+var fix = module.exports = {};
+
+fix.oneLimit = [{
+  date: '2014-01-01',
+  usage: 9,
+}, {
+  date: '2014-02-01',
+  usage: 5,
+}, {
+  date: '2014-03-01',
+  usage: 2,
+}, {
+  date: '2014-04-01',
+  usage: 5,
+}, {
+  date: '2014-05-01',
+  usage: 8,
+}, {
+  date: '2014-06-01',
+  usage: 6,
+}, {
+  date: '2014-07-01',
+  usage: 2,
+}, {
+  date: '2014-08-01',
+  usage: 3,
+}];
+
+fix.twoLimit = [{
+  date: '2014-01-01',
+  usage: 7,
+}, {
+  date: '2014-02-01',
+  usage: 3,
+}, {
+  date: '2014-03-01',
+  usage: 6,
+}, {
+  date: '2014-04-01',
+  usage: 7,
+}, {
+  date: '2014-05-01',
+  usage: 8,
+}, {
+  date: '2014-06-01',
+  usage: 1,
+}, {
+  date: '2014-07-01',
+  usage: 0,
+}, {
+  date: '2014-08-01',
+  usage: 9,
+}];
+
+fix.threeCount = [{
+  date: '2014-01-01',
+  usage: 17,
+}, {
+  date: '2014-02-01',
+  usage: 32,
+}, {
+  date: '2014-03-01',
+  usage: 26,
+}, {
+  date: '2014-04-01',
+  usage: 57,
+}, {
+  date: '2014-05-01',
+  usage: 68,
+}, {
+  date: '2014-06-01',
+  usage: 18,
+}, {
+  date: '2014-07-01',
+  usage: 90,
+}, {
+  date: '2014-08-01',
+  usage: 93,
+}];
